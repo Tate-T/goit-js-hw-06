040946966ef8c2678fa0cfe1437e204a8155c5e4
Tate-T <tatjana.tarasovych@gmail.com>
2021-09-28 20:28:33 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,42 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnCreateEl = document.querySelector('[data-action="render"]');
+const btnDeleteEl = document.querySelector('[data-action="destroy"]');
+const inputEl = document.querySelector('input');
+
+
+btnCreateEl.addEventListener("click", createBegin);
+
+function createBegin() {
+    const amount = Number(inputEl.value);
+createBoxes(amount);
+}
+
+function createBoxes(amount) {
+    const newDivx = [];
+    let currentSize = 30;
+    for (let i = 0; i < amount; i += 1) {
+        const colour = getRandomHexColor();
+
+        const divNewEl = document.createElement("div");
+
+        divNewEl.style.width = `${currentSize}px`;
+        divNewEl.style.height = `${currentSize}px`;
+        divNewEl.style.backgroundColor = `rgb(${colour[0]}, ${colour[1]}, ${colour[2]})`;
+        newDivx.push(divNewEl);
+
+        currentSize +=10;
+    }
+    divEl.append(...newDivx);
+}
+
+// function getColour() {
+//     const colourArrey = [];
+//     const firstRandomNumber = Number((Math.random() * (255 - 0) + 0).toFixed(0));
+//     const secondRandomNumber = Number((Math.random() * (255 - 0) + 0).toFixed(0));
+//     const thirdRandomNumber = Number((Math.random() * (255 - 0) + 0).toFixed(0));
+// colourArrey.push(firstRandomNumber, secondRandomNumber, thirdRandomNumber);
+// return colourArrey
+// }
