6ebab5ea960febed08d94a278b298a697d1be8cb
Tate-T <tatjana.tarasovych@gmail.com>
2021-09-28 20:09:25 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', e => {
+    e.preventDefault();
+    const {
+        elements: { email, password }
+       } = e.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('Все поля должны быть заполнены');
+    };
+
+    const user = {
+        email: email.value,
+        password: password.value,
+      };
+
+      console.log(user);
+      
+    e.currentTarget.reset();
+})
+
+   
