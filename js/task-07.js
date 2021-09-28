6f725cf61c4b91b8782a4be9d6adfc19d52bd1f5
Tate-T <tatjana.tarasovych@gmail.com>
2021-09-28 19:50:14 +0300

    task-07

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const inputRef = document.querySelector('#font-size-control');
+const textRef = document.querySelector('#text');
+
+inputRef.addEventListener('input', e => {
+    textRef.style.fontSize = `${e.target.value}px`;
+})
