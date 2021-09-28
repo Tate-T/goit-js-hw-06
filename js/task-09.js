2bf9eb2400c20681ed52ff30f213f0abadbb800a
Tate-T <tatjana.tarasovych@gmail.com>
2021-09-28 20:15:35 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorBtn = document.querySelector('.change-color');
+const colorRef = document.querySelector('.color');
+
+colorBtn.addEventListener('click', e => {
+  getRandomHexColor(colorRef)
+})
