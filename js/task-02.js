2cd5abf6c67700ae222cc14a8c2de76680fa24e4
Tate-T <tatjana.tarasovych@gmail.com>
2021-09-28 18:30:33 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+const ingredientElems = ingredients.map(ingredient => {
+const liRef = document.createElement('li');
+liRef.textContent = ingredient;
+liRef.classList.add('item');
+ return liRef;
+});
+
+ingredientsRef.append(...ingredientElems);
+console.log(ingredientElems);
